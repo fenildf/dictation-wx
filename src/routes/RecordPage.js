cbfc48caa441c6238f35ef009577ae9a0348a3c6
Kim <331599158@qq.com>
2017-10-14 17:17:56 +0800

    增加上传语音接口

--- src/routes/RecordPage.js
+++ src/routes/RecordPage.js
@@ -2,21 +2,32 @@ import React from 'react';
 import MainLayout from '../components/MainLayout/MainLayout';
 import { Icon, Button, WhiteSpace  } from 'antd-mobile';
 
+let localId;
+
 const RecordPage = ({ history }) => (
   <MainLayout history={history}>
     <WhiteSpace />
     <Button icon={require('../assets/svg/voice.svg')} onClick={() => window.wx.startRecord()}>
-      长按录音
+      开始
     </Button>
     <WhiteSpace />
     <Button icon={require('../assets/svg/voice.svg')} onClick={() => window.wx.stopRecord({
-      success: (res) => {
-        window.wx.playVoice({
-          localId: res.localId,
-        });
-      }
+      success(res) {
+        localId = res.localId
+      },
     })}>
-      停止录音
+      停止
+    </Button>
+    <Button icon={require('../assets/svg/voice.svg')} onClick={() => window.wx.playVoice({ localId })}>
+      播放
+    </Button>
+    <Button icon={require('../assets/svg/voice.svg')} onClick={() => window.wx.uploadVoice({
+      localId,
+      success(res) {
+        alert(res.serverId);
+      },
+    })}>
+      上传
     </Button>
   </MainLayout>
 );
